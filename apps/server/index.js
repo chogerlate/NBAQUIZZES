3a9fb04f53a695124aeb956aa503d88ae26fbfbe
Faan <rudfanno1@gmail.com>
2022-11-13 23:10:28 +0700

    Initial Server Folder and Workspaces

---
+++ apps/server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen("3001", ()=>{
+    console.log("HELLO SERVER");
+})
